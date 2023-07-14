294718ffb2db0b479290b842aa1f3971f5c50e16
Kareeeem <Kareem.H1715@gmail.com>
2023-07-14 06:15:04 -0700

    task0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * 
+ * Description: positive or negative or zero
+ */
+int main(void)
+{
+	if (n > 0)
+		printf("%d is positive\n",n);
+	else if (n < 0)
+		printf("%d is negative\n",n);
+	else
+		printf("%d is zero\n",n);
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+The first day with if, for and while in ALX SE
