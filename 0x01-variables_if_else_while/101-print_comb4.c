bdc090e57559468db801eb5677f923d3147ea95d
Kareeeem <Kareem.H1715@gmail.com>
2023-07-14 11:25:49 -0700

    advanced 2

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * Description: compination of three digit of numbers
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i <= '7'; i++)
+	{
+		for (j = i + 1; j <= '8'; j++)
+		{
+			for (k = j + 1; k <= '9'; k++)
+			{
+				if (j == i || i == k || j == k)
+					continue;
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == '7' && j == '8' && k == '9')
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
