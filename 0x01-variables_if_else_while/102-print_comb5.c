6a3f6afbd4ea36d91e85e086c57fbe0ab5d1ee35
Kareeeem <Kareem.H1715@gmail.com>
2023-07-14 13:09:23 -0700

    advanced 3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,14 +10,14 @@
 int main(void)
 {
 	int i, j, k, l;
-
+	l = 0;
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = '0'; j <= '8'; j++)
 		{
 			for (k = '0'; k <= '9'; k++)
 			{
-				for (l = '0'; l <= '9'; l++)
+				for (l = l; l <= '9'; l++)
 				{
 					if (i == k && j == l)
 						continue;
