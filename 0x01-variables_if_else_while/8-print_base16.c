c8ce223142ce3bcb61c930bf69483a08e597c27b
Kareeeem <Kareem.H1715@gmail.com>
2023-07-14 09:42:50 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * Description: print the hexadecimal with putchar
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i < '9'; i++)
+		putchar(i);
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
+
+	putchar('\n');
+
+	return (0);
+}
