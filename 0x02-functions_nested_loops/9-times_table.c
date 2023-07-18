1b8d1cd0a2428893a75107281ee2c52e6d38e65a
kareem <Kareem.H1715@gmail.com>
2023-07-18 23:51:08 +0300

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,32 +2,33 @@
 
 
 /**
- * main - Entry point
+ * times_table - Entry point for function timetables.
  *
  * Return: Always 0 (Success)
  *
- * Description: 
+ * Description: times table from 0 to 9.
  */
 void times_table(void)
 {
-    int i, j, result;
+	int i, j, result;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            result = i * j;
-            
-            if (result > 9)
-                _putchar(result / 10 + '0');
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
 
-            _putchar(result % 10 + '0');
-            if (j == 9)
-                break;
-            _putchar(',');
-            _putchar(' ');
-            _putchar(' ');
-        }
-        _putchar('\n');
-    }
+			if (result > 9)
+				_putchar(result / 10 + '0');
+
+			_putchar(result % 10 + '0');
+			if (j == 9)
+				break;
+			_putchar(',');
+			_putchar(' ');
+			if (result < 10)
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
 }
