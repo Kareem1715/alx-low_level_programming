b7ee513538d37818badf668cbdc86f9e0c5ba0bb
kareem <Kareem.H1715@gmail.com>
2023-07-19 01:20:08 +0300

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,20 +14,20 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
 		{
 			result = i * j;
-
-			if (result > 9)
-				_putchar(result / 10 + '0');
-
-			_putchar(result % 10 + '0');
-			if (j == 9)
-				break;
 			_putchar(',');
 			_putchar(' ');
-			if (result < 10)
+
+			if(result <= 9)
 				_putchar(' ');
+			else
+				_putchar((result / 10) + '0');
+
+			_putchar((result % 10) + '0');
+
 		}
 		_putchar('\n');
 	}
