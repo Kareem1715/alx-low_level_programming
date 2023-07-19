25e97634f8d964732ec0dffd7d387395aa95dd97
kareem <Kareem.H1715@gmail.com>
2023-07-19 19:11:55 +0300

    advanced 5

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,15 +13,15 @@ int main(void)
 	long i, prev, preprev, sum = 0;
 
 	prev = 2, preprev = 1;
-	printf("%lu, %lu, ", preprev, prev);
+	printf("%Lf, %Lf, ", preprev, prev);
 
 	for (i = 2; i <= 98; i++)
 	{
 		sum = prev + preprev;
 		if (i != 98)
-			printf("%lu, ", sum);
+			printf("%Lf, ", sum);
 		else
-			printf("%lu", sum);
+			printf("%Lf", sum);
 
 		preprev = prev;
 		prev = sum;
