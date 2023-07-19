ab978b3c364c01dd2e8de5025b400877ce2e3a3f
kareem <Kareem.H1715@gmail.com>
2023-07-19 22:18:57 +0300

    advanced 5

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -21,15 +21,14 @@ int main(void)
 		if (prev > 1000000000000000000 && preprev > 1000000000000000000)
 		{
 			printf("%lu", prev / 10000000000);
-			printf("%lu, ", prev % 1000000000);
+
 			printf("%lu", prev / 10000000000);
 			printf("%lu, ", prev % 1000000000);
 		}
 		sum = prev + preprev;
 		if (i != 98)
 			printf("%lu, ", sum);
-		else if (sum < 1000000000000000000)
-			printf("%lu", sum);
+
 
 		preprev = prev;
 		prev = sum;
