4501386db7a52e93707e5bd0ff2449b800d4756a
kareem <Kareem.H1715@gmail.com>
2023-07-19 18:52:27 +0300

    advanced 5

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * Description: Fabonacci of number
+ */
+int main(void)
+{
+	long i, prev, preprev, sum = 0;
+
+	prev = 2, preprev = 1;
+	printf("%ld, %ld, ", preprev, prev);
+
+	for (i = 2; i <= 98; i++)
+	{
+		sum = prev + preprev;
+		if (i != 98)
+			printf("%ld, ", sum);
+		else
+			printf("%ld", sum);
+
+		preprev = prev;
+		prev = sum;
+	}
+	printf("\n");
+	return (0);
+}
