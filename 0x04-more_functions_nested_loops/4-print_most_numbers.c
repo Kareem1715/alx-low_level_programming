834f1ea5109f37190f725762131fa982712bedb4
kareem <Kareem.H1715@gmail.com>
2023-07-20 16:26:05 +0300

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entery point
+ *
+ * Description: print most numbers from 0 to 9 with putchar
+ * except 4 and 6.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 4 || i == 6)
+			continue;
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
