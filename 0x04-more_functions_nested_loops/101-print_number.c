3db73cf819c140497a99578db92c5760625cf558
kareem <Kareem.H1715@gmail.com>
2023-07-21 02:05:00 +0300

    advanced 1

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,10 +15,10 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -1 * n;
+		n = n * -1;
 	}
 
-	if (n / 10)
+	if ((n / 10) > 0)
 		print_number(n / 10);
 
 	 _putchar((n % 10) + '0');
