1e65a4db5023e5f8f8366a265d46aeb1018c9e17
kareem <Kareem.H1715@gmail.com>
2023-07-20 11:49:40 +0300

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
-void positive_or_negative(int n);
+#include "main.h"
 
 /**
- * main - Entery point
- *
- * Return: Always 0 (Success)
- *
- * Description: positive or negative or zero
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	positive_or_negative(0);
+        int i;
 
-	return (0);
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
