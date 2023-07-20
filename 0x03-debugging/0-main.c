4a302e7e6fc8ae2fbf3e39b6cb182cc4ff88b4da
kareem <Kareem.H1715@gmail.com>
2023-07-20 11:18:04 +0300

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void positive_or_negative(int i);
+/**
+ * test - test the function Positive_or_negative
+ *
+ * @n: parameter of fuction
+ *
+ * Return: Always 0 (Success)
+ *
+ * Description: positive or negative or zero
+ */
+int main(void)
+{
+    positive_or_negative(0);
+
+    return (0);
+}
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * positive_or_negative - Entry point
+ *
+ * @n: parameter of fuction
+ *
+ * Return: Always 0 (Success)
+ *
+ * Description: positive or negative or zero
+ */
+void positive_or_negative(int n)
+{
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
+	return;
+}
