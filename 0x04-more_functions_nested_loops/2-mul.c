2739eda56779ff4ca3f6431b9d8d06a9aa9c8c98
kareem <Kareem.H1715@gmail.com>
2023-07-20 16:10:55 +0300

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * mul - Entery point
+ *
+ * @a: first parameter recieved by function.
+ * @b: second parameter recieved by function.
+ *
+ * Description: check the letter is digit or not.
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
