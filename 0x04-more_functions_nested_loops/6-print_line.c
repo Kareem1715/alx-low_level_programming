74eeedf18afb02ef7511561fea7616528719adf4
kareem <Kareem.H1715@gmail.com>
2023-07-20 17:52:49 +0300

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * print_line - Entery point
+ *
+ * Description: print lines ____
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (n > 0)
+			_putchar('_');
+		else
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
