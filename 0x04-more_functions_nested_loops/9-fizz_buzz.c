d83bfca35a6af35b746be8c772643dea55cea176
kareem <Kareem.H1715@gmail.com>
2023-07-20 22:26:16 +0300

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,24 +14,19 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf("Fizz ");
-			continue;
-		}
+			printf("Fizz");
+
 		else if (i % 5 == 0 && i % 3 != 0)
-		{
-			if (i == 100)
-				printf("Buzz");
-			else
-				printf("Buzz ");
-			continue;
-		}
+			printf("Buzz");
+
 		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-			continue;
-		}
-		printf("%d ", i);
+			printf("FizzBuzz");
+
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
