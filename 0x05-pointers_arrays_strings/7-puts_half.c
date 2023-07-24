a4b4183df45b3fc3b180dbabb3bc1d656c737e44
kareem <Kareem.H1715@gmail.com>
2023-07-24 21:47:34 +0300

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,7 @@ void puts_half(char *str)
 	while (str[length] != '\0')
 		length++;
 
-	for (i = length / 2; i < length; i++)
+	for (i = ((length - 1) / 2) + 1; i < length; i++)
 		_putchar (str[i]);
 
 	_putchar ('\n');
