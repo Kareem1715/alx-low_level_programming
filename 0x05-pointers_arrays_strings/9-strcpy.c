7297ac9ff6f5cd6c18ca8a72260bce157fd1e073
kareem <Kareem.H1715@gmail.com>
2023-07-25 01:43:39 +0300

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - Entery point
+ *
+ * @dest: char pointer parameter.
+ * @src: char pointer parameter.
+ *
+ * Description: copy a string.
+ *
+ * Return: nothing it is a void function.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int length, i;
+
+	length = 0;
+	while (src[length] != '\0')
+		length++;
+
+	for (i = 0; i < length; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
