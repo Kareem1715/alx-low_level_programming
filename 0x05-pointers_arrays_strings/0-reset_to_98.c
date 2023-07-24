7dd6689fa8b6c9c76feb7117859380c0c0016ebb
kareem <Kareem.H1715@gmail.com>
2023-07-24 14:45:02 +0300

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entery point
+ *
+ * @n:parameter recieved by calling function.
+ *
+ * Description: update the value of n.
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
