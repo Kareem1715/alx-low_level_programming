dea890a297071f51212653224389daacdaa8eab1
kareem <Kareem.H1715@gmail.com>
2023-07-24 19:16:06 +0300

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - Entery point
+ *
+ * @s: char pointer parameter.
+ *
+ * Description: print a string.
+ *
+ * Return: nothing it is a void function.
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+
+	j = 0;
+	for (j = i - 1; s[j] >= 0; j--)
+		_putchar (s[j]);
+
+	_putchar ('\n');
+}
