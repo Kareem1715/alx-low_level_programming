ec3f9eebefb8205b7e78c45ccb46f8cf6c390a96
kareem <Kareem.H1715@gmail.com>
2023-07-24 21:39:36 +0300

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - Entery point
+ *
+ * @str: char pointer parameter.
+ *
+ * Description: print a string.
+ *
+ * Return: nothing it is a void function.
+ */
+void puts_half(char *str)
+{
+	int i, length;
+
+	length = 0;
+	while (str[length] != '\0')
+		length++;
+
+	for (i = length / 2; i <= length; i++)
+		_putchar (str[i]);
+
+	_putchar ('\n');
+}
