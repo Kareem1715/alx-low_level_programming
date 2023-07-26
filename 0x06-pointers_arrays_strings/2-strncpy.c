de08f10f1bb0008b3941608ed67b24372fdb6015
kareem <Kareem.H1715@gmail.com>
2023-07-26 18:15:44 +0300

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -19,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 	while (dest[dest_len] != '\0')
 		dest_len++;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
 
 	return (dest);
