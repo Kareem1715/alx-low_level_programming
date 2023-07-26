0ff7ebbcebaebc4de8cda35c4f17a833b9a21894
kareem <Kareem.H1715@gmail.com>
2023-07-26 23:59:20 +0300

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - Entery point.
+ *
+ * Description: uppercase string.
+ *
+ * Return: string pointer.
+ */
+char *string_toupper(char *s)
+{
+	int lenstr, i;
+
+	lenstr = 0;
+	while (s[lenstr] != '\0')
+	lenstr++;
+
+	for (i = 0; i < lenstr; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
