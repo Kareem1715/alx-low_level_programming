7a02e9b4c115b16b38073d465cb6f01c30e9da8c
kareem <Kareem.H1715@gmail.com>
2023-07-26 21:08:23 +0300

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,16 +12,30 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, lens1, strcm;
+	int i, lens1, lens2, strcm, tmp;
 
 	lens1 = 0;
 	while (s1[lens1] != '\0')
 		lens1++;
 
+	lens2 = 0;
+	while (s2[lens2] != '\0')
+		lens2++;
+
+	if (lens2 > lens1)
+	{
+		tmp = lens2;
+		lens2 = lens1;
+		lens1 = tmp;
+	}
+
 	for (i = 0; i <= lens1; i++)
 	{
 		if (s1[i] != s2[i])
+		{
 			strcm = s1[0] - s2[0];
+			break;
+		}
 		else
 			strcm = 0;
 	}
