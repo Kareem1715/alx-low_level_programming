f08fcf310aa4a312e9b00435cb7d52dfdb64a517
kareem <Kareem.H1715@gmail.com>
2023-07-27 22:09:50 +0300

    Advanced 3

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -19,20 +19,16 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	n1len = 0;
 	while (n1[n1len] != '\0')
 	n1len++;
-
 	n2len = 0;
 	while (n2[n2len] != '\0')
 	n2len++;
-
 	if (n1len > n2len)
 		len = n1len;
 	else
 		len = n2len;
-
 	if (len + 1 > size_r)
 		return (0);
 	r[len] = '\0';
-
 	carry = 0;
 	for (i = len - 1; i >= 0; i--)
 	{
@@ -42,16 +38,13 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			n1digit = n1[n1len] - '0';
 		else
 			n1digit = 0;
-
 		if (n2len >= 0)
 			n2digit = n2[n2len] - '0';
 		else
 			n2digit = 0;
-
 		r[i] = (n1digit + n2digit + carry) % 10 + '0';
 		carry = (n1digit + n2digit + carry) / 10;
 	}
-
 	if (carry == 1)
 	{
 		r[len + 1] = '\0';
@@ -64,7 +57,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		}
 		r[0] = carry + '0';
 	}
-
-
 	return (r);
 }
