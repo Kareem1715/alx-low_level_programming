6cc81de9e6d04f4aa2dfdcbbba9e7cabed89ff2c
kareem <Kareem.H1715@gmail.com>
2023-07-31 19:22:27 +0300

    Prototypes of all functions

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_buffer - Entery point.
+ *
+ * @b: string parameter.
+ * @size: integar parameter.
+ *
+ * Description: print buffer.
+ *
+ * Return: string poniter (r).
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    
+}
