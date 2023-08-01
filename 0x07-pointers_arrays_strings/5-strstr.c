d805863ee7e063222a70083abdc9a7cfbb595934
kareem <Kareem.H1715@gmail.com>
2023-08-01 22:44:07 +0300

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,31 +1,32 @@
 #include "main.h"
 
 /**
- * _strpbrk - Entery point.
+ * _strstr - Entery point.
  *
- * @s: string parameter.
- * @accept: string parameter.
+ * @haystack: string parameter.
+ * @needle: string parameter.
  *
- * Description: searches a string for any of a set of bytes.
+ * Description: locates a substring.
  *
  * Return: Pointer.
  */
 char *_strstr(char *haystack, char *needle)
 {
-    /** 
-     * haystack = "hello, world"
-     * needle = "worldsf"
-     */
-    int i, j;
-    
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        for (j = 0; needle[j] != '\0'; j++)
-        {
-            if (haystack[i + j] != needle[j])
-                break;
- 
-        }
-    }
-    return ('\0');
+	/**
+	 * haystack = "hello, world"
+	 * needle = "worldsf"
+	 */
+	int i, j;
+
+	for (i = 0; needle[i] != 0; i++)/*world*/
+	{
+		for (j = 0; haystack[j] != 0; j++)/*hello, world*/
+		{
+			if (needle[i] == haystack[j])
+			{
+				return (haystack + j);
+			}
+		}
+	}
+	return ('\0');
 }
