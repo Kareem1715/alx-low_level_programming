09146a78f0af7803f29a533e5ae977dd69f1bc4c
kareem <Kareem.H1715@gmail.com>
2023-08-01 03:45:22 +0300

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -6,11 +6,27 @@
  * @s: string parameter.
  * @accept: string parameter.
  *
- * Description: copies memory area.
+ * Description: gets the length of a prefix substring.
  *
- * Return: locates a character in a string.
+ * Return: unsigned integar.
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    
+	int i, j, result = 0;
+
+
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				result++;
+				break;
+			}
+		}
+	}
+
+	return (result + 1);
 }
