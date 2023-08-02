b85e61a1e954e73d05a42ff612f71a2adc319937
kareem <Kareem.H1715@gmail.com>
2023-08-02 16:26:42 +0300

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Entery point.
+ *
+ * @s: string parameter.
+ *
+ * Description: prints a string
+ *
+ * Return: nothing (void function).
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*s == '\0')
+	{
+		_putchar('\0');
+		_putchar('\n');
+		return;
+	}
+	s++;
+	_puts_recursion(s);
+
+}
