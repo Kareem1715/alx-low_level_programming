6e947ca3076058996fda4d2489830507505a68e9
kareem <Kareem.H1715@gmail.com>
2023-08-02 16:52:54 +0300

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,13 +11,12 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	if (*s == '\0')
-	{
-		_putchar('\n');
+    if (*s == '\0')
+    {
+        _putchar('\n');
 		return;
-	}
-	s++;
-	_puts_recursion(s);
+    }
+    _putchar(*s);
+    _puts_recursion(s + 1);
 
 }
