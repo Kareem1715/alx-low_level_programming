e7990c5a01619207ab1e65f51d01e9337f13ee0b
kareem <Kareem.H1715@gmail.com>
2023-08-04 02:59:06 +0300

    Advanced 1

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,7 +5,7 @@
  * wildcmp - checks if two string are equal.
  *
  * @s1: string 1
- * @s2: string 2
+ * @s2: string 2 that have a wildcard *
  *
  * Description: function compares two strings
  *
@@ -15,6 +15,9 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
+
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
 
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
--- 0x08-recursion/test.c
+++ 0x08-recursion/test.c
@@ -1,38 +1,41 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Square root by recursion.
+ * is_prime_number - prime number by recursion.
  *
- * @n: integar parameter that you want to get the square root to it.
+ * @n: integar parameter you need to check it is prime or not.
  *
- * Description: This function calling the function root.
+ * Description: This function calling the function prime.
  *
- * Return: root function.
+ * Return: returns prime function.
  */
-int _sqrt_recursion(int n)
-{
-	int i = 0;
 
-	return (root(n, i));
+int is_prime_number(int n)
+{
+	int i = 2;
+
+	return (prime(n, i));
+
 }
 
 /**
- * root - Square root by recursion.
+ * prime - prime number by recursion.
  *
- * @num: integar parameter that you want to get the square root to it.
- * @j: integar parameter check if the square of number is equal to num.
+ * @num: integar parameter you need to check it is prime or not.
+ * @j: integar parameter you need to check it is prime or not.
  *
- * Description: This function returns the natural square root of a number.
+ * Description: This function check prime number.
  *
- * Return: Integar number square root.
+ * Return: returns 1 if the input is a prime number, otherwise return 0.
  */
-int root(int num, int j)
+int prime(int num, int j)
 {
-	if (j * j > num)
-		return (-1);
 
-	if (j * j == num)
-		return (j);
+	if (j >= num && num > 1)
+		return (1);
+	else if (num % j == 0 || num < 2)
+		return (0);
+	else
+		return (prime(num, j + 1));
 
-	return (root(num, j + 1));
 }
