2f75edeade678e54ea4a8d1ac1f185519f607711
kareem <Kareem.H1715@gmail.com>
2023-08-04 21:54:01 +0300

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: number of arguments passed to program.
+ * @argv: is a vector is a one dimentional array of strings.
+ *
+ * Description: prnit the name of program.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
