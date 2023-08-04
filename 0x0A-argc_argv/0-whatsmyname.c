7d421e0b5ca922b19c3b4b28a0ead4aac72bb615
kareem <Kareem.H1715@gmail.com>
2023-08-04 21:45:30 +0300

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: number of arguments passed to program.
+ * @argv: is a vector is a one dimentional array of strings.
+ *
+ * Description: prnit the name of program.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s", argv[i]);
+
+	printf("\n");
+
+	return (0);
+}
