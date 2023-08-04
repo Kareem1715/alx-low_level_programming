ab7bee383352d9615b6b5f98e0794181a4772580
kareem <Kareem.H1715@gmail.com>
2023-08-04 21:48:46 +0300

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,14 +10,9 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-		printf("%s", argv[i]);
-
-	printf("\n");
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
