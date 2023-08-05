3fb1412a3808b32def8322ef28db838de634c539
kareem <Kareem.H1715@gmail.com>
2023-08-05 03:07:36 +0300

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: number of arguments passed to program.
+ * @argv: is a vector is a one dimentional array of strings.
+ *
+ * Description: prnit the name of program.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (*(argv[i]) >= '0' && *(argv[i]) <= '9')
+			sum += atoi(argv[i]);
+
+		else
+		{
+			printf("Error\n");
+			return (0);
+		}
+
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
