11ad80a4a78fa417488e06d2accb8b930904572a
kareem <Kareem.H1715@gmail.com>
2023-08-05 03:32:25 +0300

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,25 +14,24 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc > 1)
+	if (argc < 2)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			if ((*(argv[i]) >= '0' && *(argv[i]) <= '9'))
-				sum += atoi(argv[i]);
-
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-
-		}
-		printf("%d\n", sum);		
+		printf("0\n");
+		return (0);
 	}
-	else
-		printf("%d\n", 0);
+	for (i = 1; i < argc; i++)
+	{
+		if ((*(argv[i]) >= '0' && *(argv[i]) <= '9'))
+			sum += atoi(argv[i]);
 
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+	}
+	printf("%d\n", sum);
 
 	return (0);
 }
