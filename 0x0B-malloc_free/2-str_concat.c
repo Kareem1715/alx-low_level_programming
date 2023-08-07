ccc0f9adae61e16c120c8cb3e258001fc981cb0d
kareem <Kareem.H1715@gmail.com>
2023-08-07 21:09:34 +0300

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,11 +1,37 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * reset_to_98 - Entery point
+ * str_concat - Entery point
  *
- * @n:parameter recieved by calling function.
+ * @s1: first string.
+ * @s2: the second string that will be added to frist string.
  *
- * Description: update the value of n.
+ * Description: concatenate two string with malloc.
  *
- * Return: Always 0.
+ * Return: the concatenated string.
  */
+char *str_concat(char *s1, char *s2)
+{
+	int i, s1ize = 0, s2ize = 0, Total = 0;
+	char *concat;
+
+	while (s1[s1ize] != '\0')
+		s1ize++;
+	while (s2[s2ize] != '\0')
+		s2ize++;
+
+	Total = s1ize + s2ize + 1;
+	concat = malloc(sizeof(char) * Total);
+	if (concat == 0)
+		return (NULL);
+
+	for (i = 0; i < s1ize; i++)
+		concat[i] = s1[i];
+
+	for (i = s1ize; i < Total; i++)
+		concat[i] = s2[i - s1ize];
+
+	return (concat);
+
+}
