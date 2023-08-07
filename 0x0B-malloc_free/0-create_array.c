61ec722ca074d7cf43b2b036dd7e6ebe43bfa97a
kareem <Kareem.H1715@gmail.com>
2023-08-07 17:13:38 +0300

    Putchar function

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entery point
+ *
+ * @n:parameter recieved by calling function.
+ *
+ * Description: update the value of n.
+ *
+ * Return: Always 0.
+ */
+char *create_array(unsigned int size, char c);
---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entery point
+ *
+ * @n:parameter recieved by calling function.
+ *
+ * Description: update the value of n.
+ *
+ * Return: Always 0.
+ */
