be9a1bb4c95a37bf111ed047f4bb5622adaff18d
kareem <Kareem.H1715@gmail.com>
2023-08-07 18:39:37 +0300

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,10 +17,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	arr = malloc(sizeof(char) * size);
-	if (size == 0)
+	if (size == 0 || arr == 0)
 		return ('\0');
-	if (arr == 0)
-		return ('\0');
+
 	for (i = 0; i < size; i++)
 		arr[i] = c;
 
