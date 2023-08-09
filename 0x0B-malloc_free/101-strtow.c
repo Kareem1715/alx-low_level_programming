ce53b78a4677586c33eeba040272414869916e70
kareem <Kareem.H1715@gmail.com>
2023-08-09 15:34:54 +0300

    Advanced 1

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 
+char *move_words(char *s, int be, int en);
+
 /**
  * strtow - Entery point
  *
@@ -12,31 +14,71 @@
  */
 char **strtow(char *str)
 {
-    char **words;
-    int i, j, c = 0, count = 0;
-
-    if (str == NULL || str == "")
-        return (NULL);
+	char **words;
+	int i, c = 0, count = 0, len = 0,
+	begin = 0, end = 0, mo = 0, fr = 0;
 
 	while (str[c] != '\0')
-    {
-        if (str[c] != " ")
-            count++;
+	{
+		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] != '\0'))
+			count++; /* length of charachers in words (only words not spaces) */
+		c++;
+	}
+	if (count == 0)
+		return (NULL);
 
-        c++;
-    }
-    words = malloc(sizeof(char *) * (count + 1));
-    if (words == NULL)
-        return (NULL);
-    str[count] = '\0';
+	words = malloc(sizeof(char *) * (count + 1));
+	if (words == NULL)
+		return (NULL);
 
-    for (i = 0; i < count + 1; i++)
-    {
-        for (j = 0; words[i][j] != '\0'; j++)
-        words[i] = malloc(sizeof(char) * count + 1);
+	while (str[len] != '\0')
+		len++; /* length of all string with spaces */
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+		{
+			end = i + 1;
+			words[mo] = move_words(str, begin, end);
+			if (words[mo] == NULL)
+			{
+				for (fr = 0; fr < count; fr++)
+					free(words[fr]);
+				free(words);
+				return (NULL);
+			}
+			mo++;
+		}
+		else if (str[i] != ' ' && str[i + 1] != ' ')
+			continue;
+		else
+			begin = i + 1;
+	}
+	return (words);
+}
 
-    }
+/**
+ * move_words - second function.
+ *
+ * @s: string passed by calling the function.
+ * @be: begin of the word.
+ * @en: end of the word.
+ *
+ * Description: This function move the string.
+ *
+ * Return: pointer of string.
+ */
+char *move_words(char *s, int be, int en)
+{
+	char *word;
+	int i;
 
-    return (words);
+	word = malloc(sizeof(char) * (en - be + 1));
+	if (!word)
+		return (NULL);
 
+	for (i = 0; i < en - be; i++)
+		word[i] = s[be + i];
+	word[i] = '\0';
+
+	return (word);
 }
