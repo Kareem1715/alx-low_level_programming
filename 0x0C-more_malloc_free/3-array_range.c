4c890f9986688928bbe37aa849ccf3fbc1cb4215
kareem <Kareem.H1715@gmail.com>
2023-08-10 23:38:00 +0300

    Task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - Entery point
+ *
+ * @min: minimum and first number in array.
+ * @max: maximum and last number in array.
+ *
+ * Description: make array and malloc it.
+ *
+ * Return: the pointer to the newly created array.
+ */
+int *array_range(int min, int max)
+{
+	int *arry;
+	int i, k = 0;
+
+	if (min > max)
+		return (NULL);
+	/* (max - min + 1) --> number of elements and add 1 for null termination */
+	arry = malloc(sizeof(int) * (max - min + 1));
+	if (!arry)
+		return (NULL);
+
+	for (i = min; i <= max; i++)/* moving the elements to array */
+		arry[k++] = i; /* k start form 0 and increase */
+
+	return (arry);
+}
