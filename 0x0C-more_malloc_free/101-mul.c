c61a685f8ec52bcc09a773cf56888cd9520c7fb7
kareem <Kareem.H1715@gmail.com>
2023-08-11 18:07:27 +0300

    Advanced 1

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entery point
+ *
+ * @argc: number of arguments
+ * @argv: the arguments
+ *
+ * Description: This function multiplie two number
+ *
+ * Return: Always(0) success.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+	return (0);
+
+}
