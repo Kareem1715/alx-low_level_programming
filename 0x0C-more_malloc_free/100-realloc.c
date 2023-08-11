a330b8d585eb30fa73d2f723a63dbdf2af84b849
kareem <Kareem.H1715@gmail.com>
2023-08-11 18:39:52 +0300

    Advanced 0

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -39,7 +39,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		newalloc = malloc(new_size);
 		if (!newalloc)
 			return (NULL);
-		for (i = 0; i < old_size && i < new_size; i++)
+		for (i = 0; i < old_size; i++)
 			*((char *)newalloc + 1) = *((char *)ptr + 1);
 		free(ptr);
 	}
