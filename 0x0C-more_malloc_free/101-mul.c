605f5494129a1ca0d03d4a8e411a907ff78fade6
kareem <Kareem.H1715@gmail.com>
2023-08-12 18:29:16 +0300

    Test

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int lengthOfString(char *s);
+int isDigit(char *s);
+
 /**
  * main - Entery point
  *
@@ -14,48 +17,77 @@
  */
 int main(int argc, char *argv[])
 {
-	char *pnter;
-	int i, j;
-	unsigned long mul, num1, num2;
+	int i, j, length1, length2, length, num1, num2;
+	char *mul;
 
-	if (argc != 3)
+	if (argc != 3 || isDigit(argv[1]) == 0 || isDigit(argv[2] == 0))
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
+	length1 = lengthOfString(argv[1]);
+	length2 = lengthOfString(argv[2]);
 
-	for (i = 1; i < argc; i++)
+	length = length1 + length2;
+	mul = calloc((length + 1), sizeof(int));
+	if (mul == NULL)
 	{
-		pnter = argv[i];
-		for (j = 0; pnter[j] != '\0'; j++)
+		printf("Error\n");
+		exit(98);
+	}
+	
+
+	for (i = length1 - 1; i >= 0; i--) 	  
+	{									   /* 12345 */
+		for (j = length2 - 1; j >= 0; j++) /* 67891 */
 		{
-			if (pnter[j] < '0' || pnter[j] > '9')
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			num1 = argv[1][i] - '0';
+			num2 = argv[2][j] - '0';
+
+
+
+			mul[i + j] += num1 * num2;
+			mul[i + j - 1] += mul[i + j] / 10;
+			mul[i + j] = mul[i + j] % 10; 
 		}
 	}
+	i = 0;
+	while (i < length && mul[i] == 0)
+		i++;
 
-	if (atol(argv[1]))
-		num1 = atol(argv[1]);
-	else
-	{
-		printf("Error\n");
-		exit(98);
-	}
+	if (i == length)
+		printf('0');
+	
+	for (; i < length; i++)
+		printf("%d", mul[i] + '0');
+		
+	printf("\n");
+	free(mul);
 
-	if (atol(argv[2]))
-		num2 = atol(argv[2]);
-	else
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	mul  = num1 * num2;
-
-	printf("%lu\n", mul);
 	return (0);
 
+}
+
+int lengthOfString(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	
+	return (len);
+}
+
+int isDigit(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+	}
+	
+	return (1);
 }
