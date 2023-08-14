d0a5d7b01927f2e4a44f0b9a4f5444a044454ee6
kareem <Kareem.H1715@gmail.com>
2023-08-14 21:51:36 +0300

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,15 +1,24 @@
+#include "dog.h"
+
 /**
- * struct: describe the dog
+ * init_dog: describe the dog
  * 
+ * @d: my_dog variable.
  * @name: dog name.
  * @age: dog age.
  * @owner: owner of the dog.
  *
  * Description: information about struct dog.
  */
-struct dog
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    char *name;
-    float age;
-    char *owner;
-};
+
+    if (d != 0)
+    {
+        d -> name = name;
+        d -> owner = owner;
+        d -> age = age;        
+    }
+
+    
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-
 /**
  * struct dog - describe the dog
  *
@@ -17,5 +16,9 @@ struct dog
 	float age;
 	char *owner;
 };
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 
 #endif
