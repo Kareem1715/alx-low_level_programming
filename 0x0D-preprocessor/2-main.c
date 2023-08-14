143a2fa6611439962d25640ae373fb13e8e1bdc3
kareem <Kareem.H1715@gmail.com>
2023-08-14 13:28:26 +0300

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point fuction
+ *
+ * Description: Print name of the current file.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
