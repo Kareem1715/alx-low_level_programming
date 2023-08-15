7b136689350f8621f66a51a01fce7957484028de
kareem <Kareem.H1715@gmail.com>
2023-08-15 12:25:23 +0300

    Task 0 & Task 3 & Prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,7 +10,7 @@
  *
  * Description: information about struct dog.
  */
-struct dog
+struct dog /* Task 0 */
 {
 	char *name;
 	float age;
@@ -18,11 +18,14 @@ struct dog
 };
 
 
-typedef struct dog dog_t;
+typedef struct dog dog_t; /* Task 3 */
 
+/* Protoypes of all functions */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+int strlength(char *str);
+char *strcopy(char *old, char *new);
 void free_dog(dog_t *d);
 
 #endif
