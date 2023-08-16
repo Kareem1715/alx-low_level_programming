8e74d824403409b781f509fad8e79dea74ac316e
kareem <Kareem.H1715@gmail.com>
2023-08-17 00:20:41 +0300

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,17 +1,18 @@
 #include "3-calc.h"
 
 /**
- * main - program that perfroms simple operations
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - Entry point fuction
+ *
+ * @argc: number of arguments.
+ * @argv: values of passed arguments.
+ *
+ * Description: This function performs simple operations.
  *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+	int (*func_ptr)(int, int);
 
 	if (argc != 4)
 	{
@@ -19,28 +20,18 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
-
-	func = get_op_func(argv[2]);
-
-	if (!func)
+	func_ptr = get_op_func(argv[2]);
+	if (func_ptr == NULL)
 	{
-		printf("Error\n");
+		printf("Erorr\n");
 		exit(99);
 	}
-
-	o = *argv[2];
-
-	if ((o == '/' || o == '%') && arg2 == 0)
+	if (((char)*argv[2] == '/' || (char)*argv[2] == '%') && (char)*argv[3] == '0')
 	{
-		printf("Error\n");
+		printf("Erorr\n");
 		exit(100);
 	}
 
-	result = func(arg1, arg2);
-
-	printf("%d\n", result);
-
+	printf("%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
