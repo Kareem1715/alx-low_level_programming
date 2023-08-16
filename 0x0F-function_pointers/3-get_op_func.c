1a26215e908c1aef7ee106dbde6dc434476aa0f9
kareem <Kareem.H1715@gmail.com>
2023-08-16 21:45:23 +0300

    Task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -26,7 +26,7 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (i < 6)
+	while (ops[i].op) /* last return null if no operator passed the loop */
 	{/* IF the operators equal to the sign passed */
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f); /* Return the functoin of operatro */
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,28 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[])
+/**
+ * main - Entry point fuction
+ *
+ * @argc: number of arguments.
+ * @argv: values of passed arguments.
+ *
+ * Description: This function performs simple operations.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
 {
-    int (*func_ptr)(int, int);
+	int (*func_ptr)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    func_ptr = get_op_func(argv[2]);
-    if (func_ptr == NULL)
-    {
-        printf("Erorr\n");
-        exit(99);
-    }        
-    if (atoi(argv[3]) == 0 && (*argv[2] == '/' || *argv[2] == '%'))
-    {
-        printf("Erorr\n");
-        exit(100);
-    }
-    
-    printf( "%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])) );
-    return (0);
+	func_ptr = get_op_func(argv[2]);
+	if (func_ptr == NULL)
+	{
+		printf("Erorr\n");
+		exit(99);
+	}
+	if (atoi(argv[3]) == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+	{
+		printf("Erorr\n");
+		exit(100);
+	}
+
+	printf("%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
