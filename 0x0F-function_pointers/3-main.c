8e9e694b1954665d0377019430aa88af70147d97
kareem <Kareem.H1715@gmail.com>
2023-08-16 21:38:06 +0300

    Task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+    int (*func_ptr)(int, int);
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    func_ptr = get_op_func(argv[2]);
+    if (func_ptr == NULL)
+    {
+        printf("Erorr\n");
+        exit(99);
+    }        
+    if (atoi(argv[3]) == 0 && (*argv[2] == '/' || *argv[2] == '%'))
+    {
+        printf("Erorr\n");
+        exit(100);
+    }
+    
+    printf( "%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])) );
+    return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - add function
+ *
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Description: This function add two number.
+ *
+ * Return: summation of two number.
+ */
+int op_add(int a, int b) { return (a + b); }
+
+/**
+ * op_sub - sub function
+ *
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Description: This function subtract two number.
+ *
+ * Return: subtraction of two number.
+ */
+int op_sub(int a, int b) { return (a - b); }
+
+/**
+ * op_mul - mul function
+ *
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Description: This function multiplicate two number.
+ *
+ * Return: multipication of two number.
+ */
+int op_mul(int a, int b) { return (a * b); }
+
+/**
+ * op_div - div function
+ *
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Description: This function get the divition of two number.
+ *
+ * Return: divition of two number.
+ */
+int op_div(int a, int b) { return (a / b); }
+
+/**
+ * op_mod - mod function
+ *
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Description: This function get the module of two number.
+ *
+ * Return: moudule of two number.
+ */
+int op_mod(int a, int b) { return (a % b); }
