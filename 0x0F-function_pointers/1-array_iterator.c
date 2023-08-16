e3f0ce570870c8f6bf71d9538f737ae3d12a403a
kareem <Kareem.H1715@gmail.com>
2023-08-16 18:07:01 +0300

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function pointer
+ *
+ * @array: Pass the name of array.
+ * @size: The size of passed array.
+ * @action: The function pointer that we call.
+ *
+ * Description: This function print the number of array followed by new line
+ * and print the numbers in hexadecimal.
+ *
+ * Return: Nothing it is only print.
+ */
+void array_iterator(int *array, int size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
