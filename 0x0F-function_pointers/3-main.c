b2f58976e057f6673420df9f78adf28fd2f2e585
kareem <Kareem.H1715@gmail.com>
2023-08-17 00:34:33 +0300

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,19 +19,18 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-
 	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
 	{
 		printf("Erorr\n");
 		exit(100);
 	}
-
 	func_ptr = get_op_func(argv[2]);
 	if (func_ptr == NULL)
 	{
 		printf("Erorr\n");
 		exit(99);
 	}
+
 
 	printf("%d\n", func_ptr(atoi(argv[1]), atoi(argv[3])));
 	return (0);
