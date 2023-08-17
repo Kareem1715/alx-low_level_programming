39c76436ef4b5dcb0b2c76d14eb583900dc719b1
kareem <Kareem.H1715@gmail.com>
2023-08-17 23:26:06 +0300

    Advanced

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,31 +1,40 @@
 #include "3-calc.h"
 
-int main(int argc, char* argv[])
+/**
+ * main - Entry point.
+ *
+ * @argc: The number of arguments.
+ * @argv: The values of arguments.
+ *
+ * Description: This function prints the opcodes of its own main function.
+ *
+ * Return: zero (always success)
+ */
+int main(int argc, char *argv[])
 {
-    int numByte;
-    char *ptrmain = (char *)main;
-    char *tmp;
+	int numByte, i;
+	char *ptrmain = (char *)main; /* ptrmain point to main after cast */
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
+	if (argc != 2) /* if number of passed argument not correct */
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-    numByte = atoi(argv[1]); /* convert the passed number to integar */
-    if (numByte < 0) /* if number of bytes negative */
-    {
-        printf("Error\n");
-        exit(2);
-    }
-    for (; numByte > 0; numByte--)
-    {
-        if (numByte)
-            tmp = " ";
-        else
-            tmp = "\n";
-        printf("%02hhx%s", *ptrmain, tmp);
-        ptrmain++;
-    }
-    return (0);
+	numByte = atoi(argv[1]); /* convert the passed number to integar */
+	if (numByte < 0) /* if number of bytes negative */
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < numByte; i++)
+	{/* %02hhx -> 02 for two char long, hh for lowercase and x for hexadecimal*/
+		printf("%02hhx", *ptrmain);
+		ptrmain++; /* check the next number */
+
+		if (i != numByte - 1)
+			printf(" "); /* Do space while the number is not the last number */
+	}
+	printf("\n");
+	return (0);
 }
