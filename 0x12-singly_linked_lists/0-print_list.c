0c736d30d87e3a68e8dca61828485984d391c4d2
Kareem1715 <Kareem.H1715@gmail.com>
2023-08-25 12:30:51 +0300

    Task 0 Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - print list of struct.
+ *
+ * @h: The head node passed to functoin.
+ *
+ * Description: This prints all the elements of a list_t list.
+ *
+ * Return: The number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int n = 1; /* Strat from the first node head */
+
+	if (h->str == NULL) /* If str = null */
+		printf("[%d] %s\n[%d] %s\n", 0, "(nil)", h->next->len, h->next->str);
+	else
+		printf("[%d] %s\n[%d] %s\n", h->len, h->str, h->next->len, h->next->str);
+	if (h->next) /* If h->nex node not equal null */
+		n++;
+	return (n);
+}
