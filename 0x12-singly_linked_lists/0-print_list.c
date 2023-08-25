db1845ea5e105986b8aa30678b61b053e3090bda
Kareem1715 <Kareem.H1715@gmail.com>
2023-08-25 12:40:16 +0300

    Task 0 Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,11 +13,12 @@ size_t print_list(const list_t *h)
 {
 	int n = 1; /* Strat from the first node head */
 
-	if (h->str == NULL) /* If str = null */
-		printf("[%d] %s\n[%d] %s\n", 0, "(nil)", h->next->len, h->next->str);
+	if (h->str == NULL || h->next == NULL) /* If str = null */
+		printf("[%d] %s\n", 0, "(nil)");
 	else
-		printf("[%d] %s\n[%d] %s\n", h->len, h->str, h->next->len, h->next->str);
+		printf("[%d] %s\n", h->len, h->str);
 
+	printf("[%d] %s\n", h->next->len, h->next->str);
 	n++;
 	return (n);
 }
