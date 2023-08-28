c491a1e3cc0eaff1665a6a2507bb0097e7af0c11
Kareem1715 <Kareem.H1715@gmail.com>
2023-08-28 18:38:08 +0300

    Task 2 - add node at  begin

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,7 +17,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
-
 	newNode->n = n;
 	newNode->next = *head;
 	*head = newNode;
