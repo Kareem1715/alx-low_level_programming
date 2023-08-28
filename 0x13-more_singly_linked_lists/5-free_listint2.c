a06a09c257d482397779908070579fee62446b8e
Kareem1715 <Kareem.H1715@gmail.com>
2023-08-28 21:24:36 +0300

    Task 5 - free list2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+
+/**
+ * free_listint - free a new list
+ *
+ * @head: The head node passed to functoin.
+ *
+ * Description: This function free the value of newNode
+ * and free the whole node until last node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	tmp = *head;
+	while (tmp != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+}
