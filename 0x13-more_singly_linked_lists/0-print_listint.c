8816f15740f506c2a1bef180f0efdbe5a184e2eb
Kareem1715 <Kareem.H1715@gmail.com>
2023-08-28 18:07:40 +0300

    Task 0 - print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - print list of struct.
+ *
+ * @h: The head node passed to functoin.
+ *
+ * Description: This prints all the elements of a listint_t list.
+ *
+ * Return: The number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+    size_t nodeNum = 0;
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        nodeNum++;
+    }
+    return (nodeNum);
+}
