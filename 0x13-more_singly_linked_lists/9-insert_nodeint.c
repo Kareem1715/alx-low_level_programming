129a154791ed505286f4fad247a7e0499bb68167
Kareem1715 <Kareem.H1715@gmail.com>
2023-08-29 14:02:29 +0300

    Task 9 - insert a new node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,15 +1,16 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - node index
+ * insert_nodeint_at_index - insert node
  *
  * @head: The head node passed to functoin.
- * @index: The index of the node, starting at 0.
+ * @idx: The index of the node, starting at 0.
+ * @n: The value of node you want to add to list.
  *
- * Description: This function return the node with
- * the given index to print the value of this node.
+ * Description: This function insertsa new node
+ * at a given position.
  *
- * Return: The nth node of a listint_t linked list.
+ * Return: The address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
@@ -22,9 +23,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newNode->n = n;
 	newNode->next = NULL;
 
+
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (*head);
+	}
+
 	trav = *head;
 	while (trav)
 	{
+
 		if (i == idx - 1)
 		{   /* First attach the new node with the next node in list */
 			newNode->next = trav->next;
