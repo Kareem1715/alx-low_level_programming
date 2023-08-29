32cce48ce7cc68caef1407834e1ccd3c0ee5ae12
Kareem1715 <Kareem.H1715@gmail.com>
2023-08-29 13:03:28 +0300

    Task 9 - insert a new node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (i == idx - 1)
 		{   /* First attach the new node with the next node in list */
-			newNode->next = trav->next->next;
+			newNode->next = trav->next;
 			trav->next = newNode; /* Second make the trav ptr point to newNode */
 			return (newNode);
 		}
