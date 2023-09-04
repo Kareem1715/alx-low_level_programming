301cd86cceab24d3e1cb255972718732d7055e17
Kareem1715 <Kareem.H1715@gmail.com>
2023-09-05 02:21:48 +0300

    Task 0 - read a text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * read_textfile - Textfile
+ *
+ * @filename: The name of file passed to function.
+ * @letters: The number of letters it should read and print.
+ *
+ * Description: This function reads a text file and prints it to
+ * the POSIX standard output.
+ *
+ * Return: The actual number of letters it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, sz;
+
+	if (filename == NULL)
+		return (0);
+	fd = open("filename", O_RDWR);
+	if (fd < 0)
+		return (0);
+
+	sz = write(fd, filename, letters);
+	if (sz < 0)
+		return (0);
+
+	return (sz);
+}
