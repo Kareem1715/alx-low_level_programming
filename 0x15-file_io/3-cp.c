060172a8c7d826bea1826a73c97ba1698cd43596
Kareem1715 <Kareem.H1715@gmail.com>
2023-09-05 21:44:02 +0300

    Task 3 - copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,14 +16,14 @@ int main(int ac, char **av)
 	char buff[1024];
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]), exit(97);
 
 
 	file1 = open(av[1], O_RDONLY);
 	if (file1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[0]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	file2 = open(av[2], O_RDONLY | O_CREAT | O_TRUNC, 0664);
+	file2 = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 	if (file2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
@@ -34,7 +34,7 @@ int main(int ac, char **av)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 	if (readBytes == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[0]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 	if (close(file1) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1), exit(100);
