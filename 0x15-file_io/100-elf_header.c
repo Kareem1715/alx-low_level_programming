e2e58217dce015b852b2b294b10a36031c29bb48
Kareem1715 <Kareem.H1715@gmail.com>
2023-09-06 00:46:23 +0300

    Not completed

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -2,19 +2,19 @@
 
 /**
  * main - ELF
- * 
+ *
  * @argc: number of arguments.
  * @argv: arguments values.
- * 
+ *
  * Return: success (0)
  */
 int main(int argc, __attribute__((unused)) char *argv[])
 {
-    if (argc != 2)
-    {
-        dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
-        exit(98);        
-    }
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
+		exit(98);
+	}
 
-    return (0);
+	return (0);
 }
