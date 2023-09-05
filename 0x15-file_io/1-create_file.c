1f5cafde4e02069a9283e6d2cb32550f6f19c7cc
Kareem1715 <Kareem.H1715@gmail.com>
2023-09-05 04:57:41 +0300

    Task 1 - creat a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,14 +18,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fileDescr = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
-	if (text_content == NULL || fileDescr == NULL)
+	if (text_content == NULL || fileDescr < 0)
 	{
 		close(fileDescr);
 		return (-1);
 	}
 	else
 	{
-		while (text_content[i] != NULL)
+		while (text_content[i])
 			contLen++;
 
 		sz = write(fileDescr, text_content, contLen);
