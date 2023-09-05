386674d00bfb1d44500cc574a9bbdf989d4f246e
Kareem1715 <Kareem.H1715@gmail.com>
2023-09-05 15:45:53 +0300

    Task 1 - create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,12 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (fileDescr == -1)
 		return (-1);
 
-	if (text_content == NULL)
-	{
-		close(fileDescr);
-		return (-1);
-	}
-	else
+	if (text_content != NULL)
 	{
 		while (text_content[contLen])
 			contLen++;
