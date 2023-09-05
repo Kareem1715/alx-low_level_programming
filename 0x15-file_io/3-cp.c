d8bc6b162f0a71f3b127d5d17d7f4ef415a56711
Kareem1715 <Kareem.H1715@gmail.com>
2023-09-05 22:24:10 +0300

    Task 3 - copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,49 +1,48 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - copy files
  *
  * @ac: argc number of arguments.
  * @av: argv the values of arguments.
  *
  * Description: This program copies the content of a file to another file.
+ * all errors on the POSIX standard error (STDERR_FILENO)
  *
- * Return: Always 0.
+ * Return: 0 in success and form 97 to 100 in failer.
  */
 int main(int ac, char **av)
 {
 	int writeBytes, readBytes, file1, file2;
 	char buff[1024];
 
-	if (ac != 3)
+	if (ac != 3) /* ./cp file1 file2 */
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-
-	file1 = open(av[1], O_RDONLY);
-	if (file1 == -1)
+	file1 = open(av[1], O_RDONLY); /* Read only from file1 to return number of bytes */
+	if (file1 == -1) 
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	file2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file2 == -1)
+	/* octal (0), rw- (6), rw- (6), r-- (4) */
+	file2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664); /* Write only in file2 */ 
+	if (file2 == -1) 
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
-	do {
+	/* Copy data from file1 to file2 */
 
-		readBytes = read(file1, buff, 1024);
+		readBytes = read(file1, buff, 1024); /* Read data from file1 */
 		if (readBytes == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-		writeBytes = write(file2, buff, readBytes);
+		writeBytes = write(file2, buff, readBytes); /* Write data from file1 to file2 */
 		if (writeBytes == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
-	} while (readBytes > 0);
 
-
-	if (close(file1) == -1)
+	if (close(file1) == -1) /* Close file1 and check for error */
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1), exit(100);
 
-	if (close(file2) == -1)
+	if (close(file2) == -1) /* Close file2 and check for error */
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2), exit(100);
 	return (0);
 }
