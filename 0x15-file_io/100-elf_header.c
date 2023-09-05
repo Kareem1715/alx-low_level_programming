0daa8691d67f6bb60f13af54cb0385d68d7c0586
Kareem1715 <Kareem.H1715@gmail.com>
2023-09-06 00:39:08 +0300

    Not completed

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - ELF
+ * 
+ * @argc: number of arguments.
+ * @argv: arguments values.
+ * 
+ * Return: success (0)
+ */
+int main(int argc, char *argv[])
+{
+    return (0);
+}
