5725363269b5a1bfbdeb93ede925c5de9762382b
Kareem1715 <Kareem.H1715@gmail.com>
2023-09-05 04:54:55 +0300

    Task 1 - creat a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - Textfile
+ *
+ * @filename: The name of file passed to function.
+ * @text_content: Is a NULL terminated string to write to the file.
+ *
+ * Description: This function reads a text file and prints it to
+ * the POSIX standard output.
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fileDescr, i = 0, contLen = 0, sz;
+
+	if (filename == NULL)
+		return (-1);
+	fileDescr = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
+	if (text_content == NULL || fileDescr == NULL)
+	{
+		close(fileDescr);
+		return (-1);
+	}
+	else
+	{
+		while (text_content[i] != NULL)
+			contLen++;
+
+		sz = write(fileDescr, text_content, contLen);
+		if (sz < 0)
+		{
+			close(fileDescr);
+			return (-1);
+		}
+
+	}
+
+	close(fileDescr);
+	return (1);
+}
