20c9b50b4278e29a8986daea774203ffbed0030e
Kareem1715 <Kareem.H1715@gmail.com>
2023-09-06 00:46:10 +0300

    Not completed

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,7 +8,13 @@
  * 
  * Return: success (0)
  */
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
+    if (argc != 2)
+    {
+        dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
+        exit(98);        
+    }
+
     return (0);
 }
