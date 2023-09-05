7ba43137f06fd7b1da88250fa820aebc9bc8fa3b
Kareem1715 <Kareem.H1715@gmail.com>
2023-09-05 16:22:16 +0300

    Task 1 - create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,29 +13,34 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fileDescr, contLen = 0, sz;
+	int fileDescr, contLen = 0, bytesNum;
 
 	if (filename == NULL)
 		return (-1);
 
+	/*	octal (0),  rw- (6), --- (0), --- (0) */
 	fileDescr = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fileDescr == -1)
+	if (fileDescr == -1) /* IF it fails to open the file */
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		while (text_content[contLen])
+		while (text_content[contLen] != '\0') /* Count number of char to write */
 			contLen++;
 
-		sz = write(fileDescr, text_content, contLen);
-		if (sz < 0)
+		bytesNum = write(fileDescr, text_content, contLen);
+		if (bytesNum < 0) /* Or bytesNUM == -1 */
 		{
 			close(fileDescr);
 			return (-1);
 		}
 
 	}
+	/**
+	 * If text_content == NULL --> skip the brevious if condition
+	 * and close the file without write in it (empty file).
+	 */
 
-	close(fileDescr);
+	close(fileDescr); /* At the end close the file */
 	return (1);
 }
