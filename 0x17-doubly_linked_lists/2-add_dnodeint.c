e17d44aa4c5c8c7d1cf665c1bfae4c2dfa010c33
Kareem1715 <Kareem.H1715@gmail.com>
2023-10-12 18:14:13 +0300

    Task 2 - add node at begin

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,8 +12,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	if (!*head)
-		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
@@ -22,8 +20,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->next = NULL;
 	new->prev = NULL;
 
-	new->next = *head;
-	new->prev = new->next;
+	if (*head != NULL)
+	{
+		new->next = *head;
+		(*head)->prev = new;
+	}
 	*head = new;
 
 	return (new);
