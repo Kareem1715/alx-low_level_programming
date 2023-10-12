45800633c58fad8456fad675f1c66bda2d4a3780
Kareem1715 <Kareem.H1715@gmail.com>
2023-10-12 17:42:42 +0300

    Task 2 - add node at begin

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->next = NULL;
+	new->prev = NULL;
+
 	new->next = *head;
+	new->prev = new->next;
 	*head = new;
+
 	return (new);
 }
