5bbe2bd83d5db4ba53b3722e1edce12575435aba
Kareem1715 <Kareem.H1715@gmail.com>
2023-10-12 16:34:15 +0300

    Task 1 - length of list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print list
+ * @h: head pointer point to first node
+ * Return: Number of nodes.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *trv = h;
+	size_t n = 0;
+
+	while (trv)
+		trv = trv->next, n++;
+
+	return (n);
+}
