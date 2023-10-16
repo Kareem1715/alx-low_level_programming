76408465ab77a3fe0ad2d3e287b091122f94ad1f
Kareem1715 <Kareem.H1715@gmail.com>
2023-10-16 20:31:50 +0300

    Functions Prototypes

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -22,4 +22,11 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
+/* For Advanced task 0 */
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
 #endif /* MAIN_H */
