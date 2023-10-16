488a9fc8bf7f33a9732903393f5221c13492a219
Kareem1715 <Kareem.H1715@gmail.com>
2023-10-16 20:32:18 +0300

    Advanced 0 - operation

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * add - addation function
+ *
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: addation of two number
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract function
+ *
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: subtract a from b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply function
+ *
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: multiply of two number
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divition function
+ *
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: divition of a to b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus function
+ *
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: modulus of a to b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
