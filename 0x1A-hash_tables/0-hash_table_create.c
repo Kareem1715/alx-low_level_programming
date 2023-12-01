edd1802cc5c68c5876e294497ecf7b4655f6e807
Kareem1715 <Kareem.H1715@gmail.com>
2023-12-01 23:23:19 +0200

    Task 0 - Create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Create a hash table
+ *
+ * @size: length of array
+ *
+ * Return: a pointer to the newly created hash table or NULL.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash = malloc(sizeof(hash_table_t));
+
+	hash->size = size;
+	free(hash);
+	return (hash);
+}
