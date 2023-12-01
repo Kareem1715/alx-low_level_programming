c70fc62648c33c344bdf8a3b27545f753908bc8d
Kareem1715 <Kareem.H1715@gmail.com>
2023-12-01 23:45:53 +0200

    Task 0 - Create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,8 +9,21 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash = malloc(sizeof(hash_table_t));
+	hash_table_t *hash;
+	unsigned long int i;
 
+	hash = malloc(sizeof(hash_table_t));
+	if (hash == NULL)
+		return (NULL);
+	
 	hash->size = size;
+
+	hash->array = malloc(size * sizeof(hash_node_t));
+	if (hash->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		hash->array[i] = NULL;
+
 	return (hash);
 }
